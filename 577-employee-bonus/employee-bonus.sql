336b72d5684bc9266cb5f560835c1ee154dfa72d
ashvaniverma <73066695+ashvaniverma@users.noreply.github.com>
2023-12-17 23:54:20 +0530

    Time: 1490 ms (67.34%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,2 +1,2 @@
 Select e.name,b.bonus from Employee e  LEFT JOIN  Bonus  b
-ON (e.EmpID=b.empid) where  bonus  <1000 or bonus is null; 
+ON (e.EmpID=b.empid) where  b.bonus  <1000 or bonus is null; 
