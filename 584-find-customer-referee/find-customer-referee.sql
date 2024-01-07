bce2c39fd2bdaef0cac1a9880387d911e59b510f
ashvaniverma <73066695+ashvaniverma@users.noreply.github.com>
2024-01-07 15:11:00 +0530

    Time: 1417 ms (25.85%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,2 +1,2 @@
 /* Write your PL/SQL query statement below */
-SELECT NAME FROM Customer WHERE referee_id  <>2 OR referee_id  IS NULL;
+SELECT NAME FROM Customer WHERE referee_id  <>2 OR referee_id  IS NULL ORDER BY id;
