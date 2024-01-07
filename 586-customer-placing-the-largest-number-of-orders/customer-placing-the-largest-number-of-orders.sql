300da343edf6e848822b92bdb6c80634338b0047
ashvaniverma <73066695+ashvaniverma@users.noreply.github.com>
2024-01-07 22:43:15 +0530

    Time: 3161 ms (5.11%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,7 +1,17 @@
-WITH CTE as (
+/*  WITH CTE as (
 SELECT customer_number ,COUNT(order_number)  AS NUMORDER
 FROM Orders 
 GROUP BY customer_number)
 
 SELECT CUSTOMER_NUMBER 
 FROM CTE WHERE NUMORDER = (SELECT max(NUMORDER) FROM cte);
+
+Write your PL/SQL query statement below */
+SELECT customer_number
+FROM (
+    SELECT customer_number, COUNT(order_number) AS order_count
+    FROM orders
+    GROUP BY customer_number
+    ORDER BY order_count DESC
+)
+WHERE ROWNUM = 1;
